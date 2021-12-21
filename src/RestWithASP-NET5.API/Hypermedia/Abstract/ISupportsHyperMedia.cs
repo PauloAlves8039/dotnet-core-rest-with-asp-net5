8e579b95621ee148daed536437c34859f035eed1
PauloAlves8039 <pj38alves@gmail.com>
2021-12-21 20:54:54 -0300

    Criando interfaces e constantes para uso do HATEOAS

---
+++ src/RestWithASP-NET5.API/Hypermedia/Abstract/ISupportsHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASP_NET5.API.Hypermedia.Abstract
+{
+    public interface ISupportsHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
