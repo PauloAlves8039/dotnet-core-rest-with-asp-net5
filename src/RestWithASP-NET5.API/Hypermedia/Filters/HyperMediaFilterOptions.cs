0fbf864628dd06d0d1e5a14c88fb5ddd69fdd07b
PauloAlves8039 <pj38alves@gmail.com>
2021-12-27 21:29:21 -0300

    Criando os Filters

---
+++ src/RestWithASP-NET5.API/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using RestWithASP_NET5.API.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5.API.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
