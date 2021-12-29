24f94f89d0f163cf2c89404656b789c5bd1d107c
PauloAlves8039 <pj38alves@gmail.com>
2021-12-29 20:52:50 -0300

    Implementação inicial de IUserRepository e UserVO

---
+++ src/RestWithASP-NET5.API/Repository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestWithASP_NET5.API.Data.VO;
+using RestWithASP_NET5.API.Model;
+
+namespace RestWithASP_NET5.API.Repository
+{
+    public interface IUserRepository
+    {
+        User ValidateCredentials(UserVO user);
+    }
+}
---
+++ src/RestWithASP-NET5.API/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using RestWithASP_NET5.API.Data.VO;
+using RestWithASP_NET5.API.Model;
+using RestWithASP_NET5.API.Model.Context;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestWithASP_NET5.API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _context;
+
+        public UserRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidateCredentials(UserVO user)
+        {
+            var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
+            return _context.Users.FirstOrDefault(u => (u.UserName == user.UserName) && (u.Password == pass));
+        }
+
+        private object ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
