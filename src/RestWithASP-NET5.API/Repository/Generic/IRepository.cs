cd9703841d84666b2553502ff8699b1254bbca72
PauloAlves8039 <pj38alves@gmail.com>
2022-01-05 20:04:06 -0300

    Implementando o Paged Search no PersonRepository

--- src/RestWithASP-NET5.API/Repository/Generic/IRepository.cs
+++ src/RestWithASP-NET5.API/Repository/Generic/IRepository.cs
@@ -11,5 +11,7 @@ namespace RestWithASP_NET5.API.Repository.Generic
         T Update(T item);
         void Delete(long id);
         bool Exists(long id);
+        List<T> FindWithPagedSearch(string query);
+        int GetCount(string query);
     }
 }
