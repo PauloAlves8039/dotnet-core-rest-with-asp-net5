513b6383efa871b2743320a3d146db2d522998d0
PauloAlves8039 <pj38alves@gmail.com>
2022-01-05 20:14:29 -0300

    Implementando Paged Search no PersonBusiness

--- src/RestWithASP-NET5.API/Business/IPersonBusiness.cs
+++ src/RestWithASP-NET5.API/Business/IPersonBusiness.cs
@@ -1,4 +1,5 @@
 ﻿using RestWithASP_NET5.API.Data.VO;
+using RestWithASP_NET5.API.Hypermedia.Utils;
 using System.Collections.Generic;
 
 namespace RestWithASP_NET5.API.Business
@@ -9,6 +10,7 @@ namespace RestWithASP_NET5.API.Business
         PersonVO FindById(long id);
         List<PersonVO> FindByName(string firstName, string lastName);
         List<PersonVO> FindAll();
+        PagedSearchVO<PersonVO> FindWithPagedSearch(string name, string sortDirection, int pageSize, int page);
         PersonVO Update(PersonVO person);
         PersonVO Disable(long id);
         void Delete(long id);
--- src/RestWithASP-NET5.API/Business/Implementations/PersonBusinessImplementation.cs
+++ src/RestWithASP-NET5.API/Business/Implementations/PersonBusinessImplementation.cs
@@ -1,5 +1,6 @@
 ﻿using RestWithASP_NET5.API.Data.Converter.Implementations;
 using RestWithASP_NET5.API.Data.VO;
+using RestWithASP_NET5.API.Hypermedia.Utils;
 using RestWithASP_NET5.API.Repository;
 using System.Collections.Generic;
 
@@ -19,6 +20,32 @@ namespace RestWithASP_NET5.API.Business.Implementations
         public List<PersonVO> FindAll()
         {
             return _converter.Parse(_repository.FindAll());
+        }
+
+        public PagedSearchVO<PersonVO> FindWithPagedSearch(string name, string sortDirection, int pageSize, int page)
+        {
+            var sort = (!string.IsNullOrWhiteSpace(sortDirection)) && !sortDirection.Equals("desc") ? "asc" : "desc";
+            var size = (pageSize < 1) ? 10 : pageSize;
+            var offset = page > 0 ? (page - 1) * size : 0;
+
+            string query = @"select * from person p where 1 = 1 ";
+            if (!string.IsNullOrWhiteSpace(name)) query = query + $" and p.first_name like '%{name}%' ";
+            query += $" order by p.first_name {sort} limit {size} offset {offset}";
+
+            string countQuery = @"select count(*) from person p where 1 = 1 ";
+            if (!string.IsNullOrWhiteSpace(name)) countQuery = countQuery + $" and p.first_name like '%{name}%' ";
+
+            var persons = _repository.FindWithPagedSearch(query);
+            int totalResults = _repository.GetCount(countQuery);
+
+            return new PagedSearchVO<PersonVO>
+            {
+                CurrentPage = page,
+                List = _converter.Parse(persons),
+                PageSize = size,
+                SortDirections = sort,
+                TotalResults = totalResults
+            };
         }
 
         public PersonVO FindById(long id)
