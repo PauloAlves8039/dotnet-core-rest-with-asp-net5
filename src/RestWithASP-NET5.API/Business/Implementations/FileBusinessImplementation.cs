e9c03cb7288d16c6ee229f6b4eb5fb0ca2a8aed7
PauloAlves8039 <pj38alves@gmail.com>
2022-01-06 20:24:46 -0300

    Adicionando a classe FileDetailVO e a interface IFileBusiness

---
+++ src/RestWithASP-NET5.API/Business/Implementations/FileBusinessImplementation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASP_NET5.API.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.API.Business.Implementations
+{
+    public class FileBusinessImplementation : IFileBusiness
+    {
+        public byte[] GetFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
